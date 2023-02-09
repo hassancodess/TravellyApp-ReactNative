288536c555e50ae7eb8ca4771ec863f0f178f0cb
hassancodess <dev.hassancodes@gmail.com>
2023-02-10 03:30:43 +0500

    add Line Component

---
+++ components/onboarding/Line.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Line = () => {
+  return <View className='h-1 w-1/3 bg-primary rounded-full'></View>
+}
+
+export default Line
