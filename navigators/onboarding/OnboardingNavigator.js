4b503c5be128ee9161258a61c5122d39fa458c8e
hassancodess <dev.hassancodes@gmail.com>
2023-02-10 03:31:00 +0500

    Add Onboarding/Welcome Navigator

--- navigators/onboarding/OnboardingNavigator.js
+++ navigators/onboarding/OnboardingNavigator.js
@@ -4,13 +4,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import OnboardingScreen1 from '../../screens/onboarding/OnboardingScreen1'
 import OnboardingScreen2 from '../../screens/onboarding/OnboardingScreen2'
 import OnboardingScreen3 from '../../screens/onboarding/OnboardingScreen3'
+import WelcomeScreen from '../../screens/onboarding/WelcomeScreen'
 const Stack = createNativeStackNavigator()
 const OnboardingNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName='Onboarding1'
+    >
       <Stack.Screen name='Onboarding1' component={OnboardingScreen1} />
       <Stack.Screen name='Onboarding2' component={OnboardingScreen2} />
       <Stack.Screen name='Onboarding3' component={OnboardingScreen3} />
+      <Stack.Screen name='Welcome' component={WelcomeScreen} />
     </Stack.Navigator>
   )
 }
