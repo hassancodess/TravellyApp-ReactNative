ea553b8671d458b4a092b15223ccd0704a0be759
hassancodess <dev.hassancodes@gmail.com>
2023-02-10 03:49:18 +0500

    add Container component

---
+++ components/shared/Container.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Container = ({ children }) => {
+  return <View></View>
+}
+
+export default Container
+
+const styles = StyleSheet.create({})
