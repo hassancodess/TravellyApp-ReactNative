9081f5a7167a71794404727d7b65b9cb118c7640
hassancodess <dev.hassancodes@gmail.com>
2023-02-10 20:08:51 +0500

    thank god, this thing started working as expected

--- components/onboarding/Line.js
+++ components/onboarding/Line.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import { View } from 'react-native'
+import { styled } from 'nativewind'
 
-const Line = () => {
-  return <View className='h-1 w-1/3 bg-primary rounded-full'></View>
+const StyledView = styled(View)
+
+const Line = ({ variant }) => {
+  const variantStyles = {
+    default: 'bg-white w-[33%]',
+    primary: 'bg-primary w-[33%]',
+    secondary: 'bg-secondary/100 w-[33%]',
+    primary4: 'w-[25%] bg-primary',
+    secondary4: 'bg-secondary/100 w-[25%] ml-3',
+  }
+  return (
+    <StyledView
+      className={`h-1 rounded-full ${variantStyles[variant]}`}
+    ></StyledView>
+  )
 }
 
 export default Line
