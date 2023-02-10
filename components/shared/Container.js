e677eca9c1058e243dec3d67cc960949562720c8
hassancodess <dev.hassancodes@gmail.com>
2023-02-10 17:48:20 +0500

    add custom container component

--- components/shared/Container.js
+++ components/shared/Container.js
@@ -1,8 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Container = ({ children }) => {
-  return <View></View>
+const Container = ({ children, styles }) => {
+  return (
+    <View
+      className={`flex-1 flex-col justify-between items-center px-4 my-10 ${styles}`}
+    >
+      {children}
+    </View>
+  )
 }
 
 export default Container
