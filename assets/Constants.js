9c45c626ed5b56c7282de36f9b3bf9f8f9e71473
hassancodess <dev.hassancodes@gmail.com>
2023-02-11 02:11:29 +0500

    add consonants

---
+++ assets/Constants.js
@@ -0,0 +1,16 @@
+export const colors = {
+  green700: '#01635D',
+  green500: '#089083',
+  green50: '#B7DFDB',
+  peach300: '#FEA36B',
+  peach50: '#FFDDA2',
+  links: '#3EAEFF',
+  success: '#09C933',
+  warning: '#FF941C',
+  error: '#FF3636',
+  green: '#13ce66',
+  primary: '#050505',
+  secondary: '#727272',
+  white: '#FFFFFF',
+  screen: '#FAFAFA',
+}
