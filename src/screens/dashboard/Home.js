556a7164dfbf6649ceee7077a75758c07e49f1b7
hassancodess <dev.hassancodes@gmail.com>
2023-02-14 23:24:06 +0500

    add home screen

--- src/screens/dashboard/Home.js
+++ src/screens/dashboard/Home.js
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { Text, View, TextInput } from 'react-native'
+import Container from '../../components/dashboard/Container'
+import IconButton from '../../components/shared/IconButton'
+import { colors } from '../../../assets/Constants'
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Container>
+      {/* Heading */}
+      <Text>Explore the beautiful world!</Text>
+      {/* Search */}
+      <View className='bg-white flex-row rounded-3xl pl-5 pr-1 py-1'>
+        {/* Input */}
+        <TextInput placeholder='Search' className='flex-1 text-lg' />
+        {/* Icon */}
+        <IconButton
+          icon='search-web'
+          size={35}
+          iconColor={colors.white}
+          styles={'py-3 px-3 bg-peach300'}
+        />
+      </View>
+      {/* Booking */}
+
+      <Text>Booking</Text>
+      {/* Recommended */}
+      <Text>Recommended</Text>
+      {/* Hot offers */}
+      <Text>Hot offers</Text>
+    </Container>
   )
 }
 
 export default Home
-
-const styles = StyleSheet.create({})
